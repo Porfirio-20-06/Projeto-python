26e0f789902e79d92319796617022d2afa1c48a6
Porfirio-20-06 <luisporfirio2006@gmail.com>
2026-07-13 22:30:32 -0300

    1.01

---
+++ primeiro-cod.py
@@ -0,0 +1,18 @@
+# Variavel
+
+valor_compra = 801
+
+if valor_compra > 1000:
+    print('frete gratis')
+
+elif valor_compra > 800:
+    print('frete com 80% de desconto')
+
+elif valor_compra > 400:
+    print('frete com 50% de desconto')
+
+elif valor_compra > 200:
+    print('frete com 15% de desconto')
+
+else:
+    print('frete sem desconto')
